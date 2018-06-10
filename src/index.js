28a25b209e1c3a21455d94f41206fa6a3a27f7bd
Leo Q <leoq91@gmail.com>
2018-06-10 10:08:47 -0700

    keep heroku awake

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import configureStore, {history} from './store/configureStore';
 import './styles/styles.css';
 import App from './App';
 
+const http = require('http');
+setInterval(function() {
+  http.get('https://resort-viewer.herokuapp.com/');
+}, 300000);
+
 const target = document.querySelector('#root');
 
 const store = configureStore();
