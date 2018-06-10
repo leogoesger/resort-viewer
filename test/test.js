2a74e7321b1b0c20812a2bd09a0a067eab766303
Leo Q <leoq91@gmail.com>
2018-06-10 14:35:53 -0700

    add e2e to ci

--- test/test.js
+++ test/test.js
@@ -1,40 +1,38 @@
 const Nightmare = require('nightmare');
 
-describe('Load a Page', function() {
-  this.timeout('30s');
+describe('Load pages', function() {
+  this.timeout('10s');
 
   let nightmare = null;
   beforeEach(() => {
     nightmare = new Nightmare();
   });
 
-  describe('/ (Pages)', () => {
-    it('upload page should load without error', done => {
-      nightmare
-        .goto('http://localhost:3000/')
-        .end()
-        .then(function(result) {
-          done();
-        })
-        .catch(done);
-    });
-    it('about page should load without error', done => {
-      nightmare
-        .goto('http://localhost:3000/about')
-        .end()
-        .then(function(result) {
-          done();
-        })
-        .catch(done);
-    });
-    it('restore page should load without error', done => {
-      nightmare
-        .goto('http://localhost:3000/resorts')
-        .end()
-        .then(function(result) {
-          done();
-        })
-        .catch(done);
-    });
+  it('upload page should load without error', done => {
+    nightmare
+      .goto('https://resort-viewer.herokuapp.com/')
+      .end()
+      .then(function(result) {
+        done();
+      })
+      .catch(done);
+  });
+  it('about page should load without error', done => {
+    nightmare
+      .goto('https://resort-viewer.herokuapp.com/about')
+      .end()
+      .then(function(result) {
+        done();
+      })
+      .catch(done);
+  });
+  it('restore page should load without error', done => {
+    nightmare
+      .goto('https://resort-viewer.herokuapp.com/resorts')
+      .end()
+      .then(function(result) {
+        done();
+      })
+      .catch(done);
   });
 });
